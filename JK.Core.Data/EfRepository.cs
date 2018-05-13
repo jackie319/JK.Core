d004885a87d912164671dace69da64ae5a07706e
jackie319 <jackie319@vip.qq.com>
2018-05-13 15:34:59 +0800

    data

---
+++ JK.Core.Data/EfRepository.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using JK.Core.Core;
+using JK.Core.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace JK.Core.Data
+{
+    /// <summary>
+    /// 2017/11/11 不再继承自BaseEntity（IRepository<T> 取消了继承自BaseEntity）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EfRepository<T> : IRepository<T> where T : class //:BaseEntity
+    {
+        #region Fields
+
+        private readonly IDbContext _context;
+        private DbSet<T> _entities;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="context">Object context</param>
+        public EfRepository(IDbContext context)
+        {
+            this._context = context;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Get full error
+        /// </summary>
+        /// <param name="exc">Exception</param>
+        /// <returns>Error</returns>
+        //protected string GetFullErrorText(DbEntityValidationException exc)
+        //{
+        //    var msg = string.Empty;
+        //    foreach (var validationErrors in exc.EntityValidationErrors)
+        //        foreach (var error in validationErrors.ValidationErrors)
+        //            msg += string.Format("Property: {0} Error: {1}", error.PropertyName, error.ErrorMessage) + Environment.NewLine;
+        //    return msg;
+        //}
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Get entity by identifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        public virtual T GetById(object id)
+        {
+            //see some suggested performance optimization (not tested)
+            //http://stackoverflow.com/questions/11686225/dbset-find-method-ridiculously-slow-compared-to-singleordefault-on-id/11688189#comment34876113_11688189
+            return this.Entities.Find(id);
+        }
+
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Insert(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                this.Entities.Add(entity);
+
+                this._context.SaveChanges();
+            }
+           // catch (DbEntityValidationException dbEx)
+             catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        /// <summary>
+        /// Insert entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual void Insert(IEnumerable<T> entities)
+        {
+            try
+            {
+                if (entities == null)
+                    throw new ArgumentNullException("entities");
+
+                foreach (var entity in entities)
+                    this.Entities.Add(entity);
+
+                this._context.SaveChanges();
+            }
+            catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Update(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                this._context.SaveChanges();
+            }
+            catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        /// <summary>
+        /// Update entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual void Update(IEnumerable<T> entities)
+        {
+            try
+            {
+                if (entities == null)
+                    throw new ArgumentNullException("entities");
+
+                this._context.SaveChanges();
+            }
+            catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Delete(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
+
+                this.Entities.Remove(entity);
+
+                this._context.SaveChanges();
+            }
+            catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        /// <summary>
+        /// Delete entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual void Delete(IEnumerable<T> entities)
+        {
+            try
+            {
+                if (entities == null)
+                    throw new ArgumentNullException("entities");
+
+                foreach (var entity in entities)
+                    this.Entities.Remove(entity);
+
+                this._context.SaveChanges();
+            }
+            catch (Exception dbEx)
+            {
+                throw new Exception(dbEx.Message, dbEx);
+            }
+        }
+
+        #region 20171119  add  by jk
+        /// <summary>
+        /// 20171119  add  by jk
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public IQueryable<T> Where(Expression<Func<T, bool>> exp)
+        {
+           return  Table.WhereBy(exp);
+        }
+
+        public IQueryable<T> WherePage(Expression<Func<T, bool>> exp, QueryBase query,out int total)
+        {
+            var queryAll = Table.WhereBy(exp);
+            total = queryAll.Count();
+            return queryAll.Skip(query.Skip).Take(query.Take);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> exp, IList<OrderExpressionStruct> structList)
+        {
+            return Table.WhereBy(exp).OrderBy(structList);
+        }
+
+        public IQueryable<T> WherePage(Expression<Func<T, bool>> exp, IList<OrderExpressionStruct> structList, QueryBase query,out int total)
+        {
+            var queryAll = Table.WhereBy(exp);
+            total = queryAll.Count();
+            return queryAll.OrderBy(structList).Skip(query.Skip).Take(query.Take);
+        }
+        #endregion
+
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a table
+        /// </summary>
+        public virtual IQueryable<T> Table
+        {
+            get
+            {
+                return this.Entities;
+            }
+        }
+
+        /// <summary>
+        /// Gets a table with "no tracking" enabled (EF feature) Use it only when you load record(s) only for read-only operations
+        /// </summary>
+        public virtual IQueryable<T> TableNoTracking
+        {
+            get
+            {
+                return this.Entities.AsNoTracking();
+            }
+        }
+
+        /// <summary>
+        /// Entities
+        /// </summary>
+        protected virtual DbSet<T> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                    _entities = _context.Set<T>();
+                return _entities;
+            }
+        }
+
+        #endregion
+    }
+}
