1bd87e6c2912806ff20bbcbdb90b767637f1a484
jackie319 <jackie319@vip.qq.com>
2018-05-14 18:20:04 +0800

    data

---
+++ JK.Core.Data/DbContextGetter/DbContextGetter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Core.Data
+{
+    //public class DbContextGetter:IDbContextGetter
+    //{
+    //    //public T GetByName<T>(string name)
+    //    //{
+    //    //    return AutofacDependencyResolver.Current.RequestLifetimeScope.ResolveNamed<T>(name);
+    //    //}
+    //}
+}
