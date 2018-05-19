4155b006c6466c07ca79affbbc562c7f7a3d3087
jackie319 <jackie319@vip.qq.com>
2018-05-19 16:56:06 +0800

    dbset

---
+++ JK.Core.Core/Config/JKConfigHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace JK.Core.Core.Config
+{
+    //public class JKConfigHandler : IConfigurationSectionHandler
+    //{
+    //    private static Dictionary<string, string> _dictionary;
+    //    public object Create(object parent, object configContext, XmlNode section)
+    //    {
+    //        Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+    //        if (section != null)
+    //        {
+    //            foreach (XmlNode node in section.ChildNodes)
+    //            {
+    //                if (node.Name.Equals("conStr"))
+    //                {
+    //                    foreach (XmlNode childNode in node)
+    //                    {
+    //                        try
+    //                        {
+    //                            var name = childNode.Attributes["name"].InnerText;
+    //                            var value = childNode.Attributes["value"].InnerText;
+    //                            var text = childNode.Attributes["text"].InnerText;
+    //                            myDictionary.Add(name, value);
+    //                        }
+    //                        catch (NullReferenceException)
+    //                        {
+    //                            throw  new CommonException("config文件配置错误，找不到节点");
+    //                        }
+                         
+                           
+    //                    }
+    //                }
+
+    //                if (node.Name.Equals("keyValue"))
+    //                {
+
+    //                }
+
+    //            }
+    //        }
+    //        _dictionary = myDictionary;
+    //        return _dictionary;
+    //    }
+
+    //    public static string GetValue(string key)
+    //    {
+    //        return _dictionary[key];
+    //    }
+    //}
+}
---
+++ JK.Core.Core/Plugins/PluginDescriptor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JK.Core.Core.Plugins
+{
+    //public class PluginDescriptor
+    //{
+    //    public virtual string PluginFileName { get; set; }
+
+    //    /// <summary>
+    //    /// Plugin type
+    //    /// </summary>
+    //    public virtual Type PluginType { get; set; }
+
+    //    /// <summary>
+    //    /// The assembly that has been shadow copied that is active in the application
+    //    /// </summary>
+    //    public virtual Assembly ReferencedAssembly { get; internal set; }
+
+    //    /// <summary>
+    //    /// The original assembly file that a shadow copy was made from it
+    //    /// </summary>
+    //    public virtual FileInfo OriginalAssemblyFile { get; internal set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the plugin group
+    //    /// </summary>
+    //    public virtual string Group { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the friendly name
+    //    /// </summary>
+    //    public virtual string FriendlyName { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the system name
+    //    /// </summary>
+    //    public virtual string SystemName { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the version
+    //    /// </summary>
+    //    public virtual string Version { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the supported versions of nopCommerce
+    //    /// </summary>
+    //    public virtual IList<string> SupportedVersions { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the author
+    //    /// </summary>
+    //    public virtual string Author { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the display order
+    //    /// </summary>
+    //    public virtual int DisplayOrder { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the list of store identifiers in which this plugin is available. If empty, then this plugin is available in all stores
+    //    /// </summary>
+    //    public virtual IList<int> LimitedToStores { get; set; }
+
+    //    /// <summary>
+    //    /// Gets or sets the value indicating whether plugin is installed
+    //    /// </summary>
+    //    public virtual bool Installed { get; set; }
+
+    //    public virtual T Instance<T>() where T : class, IPlugin
+    //    {
+    //        object instance;
+
+    //        var builder = new ContainerBuilder();                    //autofac container
+    //        var container = builder.Build();                         //
+    //        var _containerManager = new ContainerManager(container);//todo:此处只是示例。需要放在适当的时机
+
+    //        if (!_containerManager.TryResolve(PluginType, null, out instance))
+    //        {
+    //            //not resolved
+    //            instance = _containerManager.ResolveUnregistered(PluginType);
+    //        }
+    //        var typedInstance = instance as T;
+    //        if (typedInstance != null)
+    //            typedInstance.PluginDescriptor = this;
+    //        return typedInstance;
+    //    }
+    //    public IPlugin Instance()
+    //    {
+    //        return Instance<IPlugin>();
+    //    }
+
+   
+
+
+    //}
+}
